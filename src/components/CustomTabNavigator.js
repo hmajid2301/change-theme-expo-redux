ba4252a2db772576d382f74d89a49b5695d61ca8
Haseeb Majid <hmajid2301@gmail.com>
2018-11-27 21:01:19 +0000

    :sparkles: Toggle Button changes with Tabs
    The toggle button colour now changes when the tab changes colour.

--- src/components/CustomTabNavigator.js
+++ src/components/CustomTabNavigator.js
@@ -1,8 +1,12 @@
+import React, { Component } from 'react';
+import store from '../store';
 import { createMaterialTopTabNavigator } from 'react-navigation';
 
 import PageA from '../screens/PageA';
 import PageB from '../screens/PageB';
 import { COLORS } from '../themes';
+import { toggleTheme } from '../actions';
+import { connect } from 'react-redux';
 
 
 const commonTabOptions = color => ({
@@ -14,12 +18,22 @@ const commonTabOptions = color => ({
   },
 });
 
+const commonTabPress = (navigation, defaultHandler, color) => {
+  toggleTheme(color)
+  defaultHandler();
+}
+
 const CustomerTabNavigator = createMaterialTopTabNavigator({
   Encoding: {
     screen: PageA,
     navigationOptions: {
       tabBarLabel: 'A',
       tabBarOptions: commonTabOptions(COLORS.red.hexCode),
+      tabBarOnPress: ({ navigation, defaultHandler }) => {
+        store.dispatch(toggleTheme(COLORS.blue));
+        console.log("HELLO A");
+        defaultHandler();
+      },
     },
   },
   Decoding: {
@@ -27,11 +41,24 @@ const CustomerTabNavigator = createMaterialTopTabNavigator({
     navigationOptions: {
       tabBarLabel: 'B',
       tabBarOptions: commonTabOptions(COLORS.blue.hexCode),
+      tabBarOnPress: ({ navigation, defaultHandler }) => {
+        store.dispatch(toggleTheme(COLORS.red));
+        console.log("HELLO B");
+        defaultHandler();
+      },
     },
   },
 }, {
-  tabBarPosition: 'bottom',
+    tabBarPosition: 'bottom',
 });
 
 
-export default CustomerTabNavigator;
+const mapStateToProps = state => ({
+  color: state.Theme.colorData,
+});
+
+const mapDispatchToProps = dispatch => ({
+  toggleTheme: color => dispatch(toggleTheme(color)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CustomerTabNavigator);
